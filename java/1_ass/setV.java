26562fced825a1f6f87f80a038af4546e49c8fa1
Ratna Priya <ratnpriya1998@gmail.com>
2019-07-01 08:13:55 +0530

    Add files via upload

---
+++ java/1_ass/setV.java
@@ -0,0 +1,21 @@
+public class setV{
+
+ public static void main(String[] args) {
+ 
+ int n = 56;
+ setVal(n);
+ 
+ 
+ 
+ }
+ 
+  public static void setVal(int num){
+   display(num);
+  }
+ 
+  public static void display(int num){
+ 
+  System.out.printf("The data passed is "+ num+"\n");
+
+}
+}
