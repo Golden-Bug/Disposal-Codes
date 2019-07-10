1bd444a649bc8ed646a511dd16ec662bbf22a096
Ratna Priya <ratnpriya1998@gmail.com>
2019-07-10 12:41:54 +0530

    Add files via upload

---
+++ op_lab/shell/list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "What is your name ?"
+read name
+echo "Enter an remark"
+read remark
+echo "OK ! Your name is $name"
+echo "$remark."
+
+echo "Where are you ?"
+pwd 
+
+echo "File running now"
+ls -lt
+
+# We can write any terminal command
+
+
+
+
+
+
